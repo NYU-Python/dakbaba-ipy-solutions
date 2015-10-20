7fc36e032ce1d7f5f828d38384f961360721d2ba
gotdairyya <gotdairyya@users.noreply.github.com>
2015-10-20 13:12:04 -0400

    Update dakbaba-2.1.py
    added validation of arguments

--- dakbaba-2.1.py
+++ dakbaba-2.1.py
@@ -12,16 +12,23 @@ args = sys.argv[1:]
 #initialize dictionary
 argdict = {}
 
+#create dictionary
 for item in args:
 	elements = item.split('=')
 	argdict[elements[0]] = elements[1]
 
 
-#check that required fields are there
-missing = required_args.difference(argdict)
+#check to see that entered fields are valid
+entered_args = set([argdict.keys()])
+if valid_args.issuperset(entered_args):
+	print 'Valid arguments!"
+else:
+	sys.exit('ERROR: Info missing. Please try again')
 
+#check that required fields are there
+missing_required_args = required_args.difference(argdict)
 if len(missing) > 0:
-	print 'ERROR: Info missing. Please try again'
+	sys.exit('ERROR: Info missing. Please try again')
 
 template = '''From: {0}
 To: {1}
