85262ffff72486f9d9adfac43f507360adb58ec3
gotdairyya <gotdairyya@users.noreply.github.com>
2015-10-27 16:35:47 -0400

    Update mylib.py
    took argparse out of the script
    please see 4.1 solutions

--- mylib.py
+++ mylib.py
@@ -1,55 +1,41 @@
 #!/usr/bin/env python
 
-import argparse
+import sys
 import os
 import time
+
 
 #The priority levels should probably be converted to an integer scale
 class Logger(object):
 	def __init__(self, filename, priority, datetime, scriptname):
-		parser = argparse.ArgumentParser()
-
-		parser.add_argument('priority',  action ='store',default='DEBUG', help='What priority is this doc?')
-		parser.add_argument('datetime', action='store', default='TRUE', help='Do you want to log the date and time?')
-		parser.add_argument('scriptname', action='store', default='TRUE', help='Do you want to log the file name?')
-
-		args = parser.parse_args()
-
-
-		filename = os.path.basename(args[0])
-		priority = args[1].upper()
-		priorities = {'DEBUG': 1, 'INFO': 2, 'WARNING': 3, 'ERROR': 4, 'CRITICAL': 5}
-		priority = int(priorities.get(priority))
-		write_date = args[2]
-		write_scriptname = args[3]
-
+		pass
 
 	#may not need number in write_log
 	def debug(self, msg):
 		if priority >= 1: 
 			self.write_log(msg, 1)
 		else:
-			print "Can't run"
+			print ("Can't run")
 	def info(self, msg):
 		if priority >= 2:
 			self.write_log(msg, 2)
 		else:
-			print "Can't run"
+			print ("Can't run")
 	def warning(self, msg):
 		if priority >= 3:
 			self.write_log(msg, 3)
 		else:
-			print "Can't run"
+			print ("Can't run")
 	def error(self, msg):
 		if priority >= 4:
 			self.write_log(msg, 4)
 		else:
-			print "Can't run"	
+			print ("Can't run")	
 	def critical(self, msg):
 		if priority == 5:
 			self.write_log(msg, 5)
 		else:
-			print "Can't run"
+			print ("Can't run")
 
 	#this will take in the priorities argument, and go from there
 	#calls compose_prepend() to retrieve data and/por filename
@@ -71,3 +57,4 @@ class Logger(object):
 		if write_scriptname:
 			#prepend file with
 			fh.write(filename + log_line_spacer)
+		fh.close()
