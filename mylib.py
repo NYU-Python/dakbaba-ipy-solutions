199467dba6bd8afc26344ad5cef1a5fcf151ba78
gotdairyya <gotdairyya@users.noreply.github.com>
2015-10-28 12:05:02 -0400

    Update mylib.py
    works.
    not sure if a text file is created though...

--- mylib.py
+++ mylib.py
@@ -8,31 +8,34 @@ import time
 #The priority levels should probably be converted to an integer scale
 class Logger(object):
 	def __init__(self, filename, priority, datetime, scriptname):
-		pass
+		self.filename = filename
+		self.priority = priority
+		self.datetime = datetime
+		self.scriptname = scriptname
 
 	#may not need number in write_log
 	def debug(self, msg):
-		if priority >= 1: 
+		if self.priority >= 1: 
 			self.write_log(msg, 1)
 		else:
 			print ("Can't run")
 	def info(self, msg):
-		if priority >= 2:
+		if self.priority >= 2:
 			self.write_log(msg, 2)
 		else:
 			print ("Can't run")
 	def warning(self, msg):
-		if priority >= 3:
+		if self.priority >= 3:
 			self.write_log(msg, 3)
 		else:
 			print ("Can't run")
 	def error(self, msg):
-		if priority >= 4:
+		if self.priority >= 4:
 			self.write_log(msg, 4)
 		else:
 			print ("Can't run")	
 	def critical(self, msg):
-		if priority == 5:
+		if self.priority == 5:
 			self.write_log(msg, 5)
 		else:
 			print ("Can't run")
@@ -41,20 +44,20 @@ class Logger(object):
 	#calls compose_prepend() to retrieve data and/por filename
 	#opens the file and write the log line
 	def write_log(self, msg, priorities):
-		self.compose_prepend()
+		self.compose_prepend
 
 
 	#checks the instance to see whether a date and/or filename are desired
 	def compose_prepend():
 		log_line_spacer = "    "
 		#open file
-		fh = open(filename, 'w')
+		fh = open(self.filename, 'w')
 		#write msg in file
-		if write_date:
+		if self.datetime:
 			#prepend file with
 			time_date =  time.ctime()
 			fh.write(time_date + log_line_spacer)
-		if write_scriptname:
+		if self.scriptname:
 			#prepend file with
-			fh.write(filename + log_line_spacer)
+			fh.write(self.filename + log_line_spacer)
 		fh.close()
