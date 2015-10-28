334e23fa72f70cc89f3c1f7960f8a0084fd79c16
gotdairyya <gotdairyya@users.noreply.github.com>
2015-10-28 12:54:41 -0400

    Update mylib.py
    I keep getting this error! Does anyone know what it means?
    
    Traceback (most recent call last):
      File "dakbaba-4.1.py", line 27, in <module>
        mylog.debug('will log if priority is DEBUG only')
      File "/Users/deryaakbaba-locus/Python/mylib.py", line 20, in debug
        self.write_log(msg, 1)
      File "/Users/deryaakbaba-locus/Python/mylib.py", line 48, in write_log
        self.compose_prepend()
      File "/Users/deryaakbaba-locus/Python/mylib.py", line 61, in compose_prepend
        fh.writelines(time_date)
    TypeError: 'str' does not support the buffer interface

--- mylib.py
+++ mylib.py
@@ -9,6 +9,7 @@ import time
 class Logger(object):
 	def __init__(self, filename, priority, datetime, scriptname):
 		self.filename = filename
+		print (self.filename)
 		self.priority = priority
 		self.datetime = datetime
 		self.scriptname = scriptname
@@ -18,46 +19,49 @@ class Logger(object):
 		if self.priority >= 1: 
 			self.write_log(msg, 1)
 		else:
-			print ("Can't run")
+			print ("Can't run debug")
 	def info(self, msg):
 		if self.priority >= 2:
 			self.write_log(msg, 2)
 		else:
-			print ("Can't run")
+			print ("Can't run info")
 	def warning(self, msg):
 		if self.priority >= 3:
 			self.write_log(msg, 3)
 		else:
-			print ("Can't run")
+			print ("Can't run warning")
 	def error(self, msg):
 		if self.priority >= 4:
 			self.write_log(msg, 4)
 		else:
-			print ("Can't run")	
+			print ("Can't run error")	
 	def critical(self, msg):
 		if self.priority == 5:
 			self.write_log(msg, 5)
 		else:
-			print ("Can't run")
+			print ("Can't run critical")
 
 	#this will take in the priorities argument, and go from there
 	#calls compose_prepend() to retrieve data and/por filename
 	#opens the file and write the log line
 	def write_log(self, msg, priorities):
-		self.compose_prepend
+		self.compose_prepend()
 
 
 	#checks the instance to see whether a date and/or filename are desired
-	def compose_prepend():
+	def compose_prepend(self):
 		log_line_spacer = "    "
 		#open file
-		fh = open(self.filename, 'w')
+		fh = open(self.filename, 'wb')
 		#write msg in file
 		if self.datetime:
 			#prepend file with
 			time_date =  time.ctime()
-			fh.write(time_date + log_line_spacer)
+			fh.writelines(time_date + log_line_spacer)
 		if self.scriptname:
 			#prepend file with
-			fh.write(self.filename + log_line_spacer)
+			fh.writelines(self.filename + log_line_spacer)
+		
+		#print results
+		print (fh.read())
 		fh.close()
