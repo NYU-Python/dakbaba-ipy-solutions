535dad572f2174119a112eebf99c2d05c357a3e8
gotdairyya <gotdairyya@users.noreply.github.com>
2015-10-29 12:50:51 -0400

    Update dakbaba-4.1.py
    after rich's comments

--- dakbaba-4.1.py
+++ dakbaba-4.1.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python
 
-import sys
-import argparse
-import os
-import time
+import sys, argparse, os, time
 
 parser = argparse.ArgumentParser("Please list the file you would like to open, the priority level, whether you want a timestamp and filename on new text")
-parser.add_argument('--filename', action ='store', help='What file would you like to log too?')
-parser.add_argument('--priority',  action ='store',choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], default='DEBUG', help='What priority is this doc?')
-parser.add_argument('--datetime', action='store', default='TRUE', help='Do you want to log the date and time?')
-parser.add_argument('--scriptname', action='store', default='TRUE', help='Do you want to log the file name?')
+parser.add_argument('--filename', help='What file would you like to log too?')
+parser.add_argument('--priority', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], default='DEBUG', help='What priority is this doc?')
+parser.add_argument('--datetime', default='TRUE', help='Do you want to log the date and time?')
+parser.add_argument('--scriptname', default='TRUE', help='Do you want to log the file name?')
 
 args = parser.parse_args()
 
 filename = args.filename
 priority = args.priority
 priorities = {'DEBUG': 1, 'INFO': 2, 'WARNING': 3, 'ERROR': 4, 'CRITICAL': 5}
-priority = int(priorities.get(priority))
+priority = priorities.get(priority)
 datetime = args.datetime
 scriptname = args.scriptname
 
